3ad8c3263b4b07a8c2396836f2db7dd0ac561e6c
jasonwhitehead <jasonwhitehad@cvpcorp.com>
2021-07-19 12:49:45 -0500

    setup & configure static storybook site to publish to github pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.scss";
 function App() {
   return (
     <>
-      <h1 className="margin-5">Welcome to the Easey Design System & Component Library</h1>
+      <h1 className="margin-5 bg-base-lighter">Welcome to the Easey Design System & Component Library</h1>
     </>
   );
 }
