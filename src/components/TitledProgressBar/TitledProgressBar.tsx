9f258f43e7096aa6c364bb4914f917bd59f3932b
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-01-26 15:54:15 -0500

    fix: made custom progress bar implementation
    Made a custom progress bar implementation

--- src/components/TitledProgressBar/TitledProgressBar.tsx
+++ src/components/TitledProgressBar/TitledProgressBar.tsx
@@ -1,4 +1,3 @@
-import ProgressBar from "@ramonak/react-progress-bar";
 import './TitledProgressBar.scss';
 
 export interface TitledProgressBarProps {
@@ -9,15 +8,14 @@ export interface TitledProgressBarProps {
 
 export const TitledProgressBar = ({ percentage, title, lastUpdated }: TitledProgressBarProps): JSX.Element => {
   return (
-    <div className="easey-progress-bar" >
-      <ProgressBar className = "wrapper" completed = {percentage} barContainerClassName="barContainer" bgColor="#005ea2" labelSize="25px" height="70px"></ProgressBar>
+    <div className="easey-progress-bar">
+      <div className="wrapper">
+        <div style={{ width: percentage + '%' }} className="progressFill"></div>
+        <span className="progressText">{percentage + '%'}</span>
+      </div>
       <div className="title">
-        <p className="text-bold margin-0 mainTitleText">
-          {title}
-        </p>
-        <p className="margin-0 padding-y-1 line-height-mono-1 subTitleText">
-          {lastUpdated}
-        </p>
+        <p className="text-bold margin-0 mainTitleText">{title}</p>
+        <p className="margin-0 padding-y-1 line-height-mono-1 subTitleText">{lastUpdated}</p>
       </div>
     </div>
   );
