6e7bff29616b88c5904a00e564d680c8e63347ee
Sejal Dhawan <sejaldhawan@cvpcorp.com>
2022-01-28 11:46:07 -0500

    fix(what is new box): allowed title to be specified

--- src/components/WhatIsNewBox/WhatIsNewBox.tsx
+++ src/components/WhatIsNewBox/WhatIsNewBox.tsx
@@ -1,14 +1,15 @@
 import './WhatIsNewBox.scss';
 
 export interface WhatIsNewBoxProps {
+  title: string;
   text: string;
 }
 
-export const WhatIsNewBox = ({ text }: WhatIsNewBoxProps): JSX.Element => {
+export const WhatIsNewBox = ({ title, text }: WhatIsNewBoxProps): JSX.Element => {
   return (
     <div className="whatIsNewBox">
       <div className="box border-right-1px border-left-1px border-bottom">
-        <div className="title font-sans-xl text-bold text-white text-center padding-y-2">What's New</div>
+        <div className="title font-sans-xl text-bold text-white text-center padding-y-2"> {title} </div>
         <div className="padding-2"> {text}</div>
       </div>
     </div>
