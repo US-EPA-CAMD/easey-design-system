a2d467ae40f2ec72293fe8c481a4118e1770eb22
Jon Maxwell Diebold <max.diebold@erg.com>
2024-07-14 00:47:41 -0400

    Added styling to preloader

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -35,7 +35,7 @@ export const Preloader = ({
 
   return (
     <div className="text-center" aria-live="polite">
-      <p>
+      <p className="margin-0 display-flex flex-align-center">
         {animationOn ? (
           <img alt="Content loading" title="Content loading" src={loadingSpinner} />
         ) : (
