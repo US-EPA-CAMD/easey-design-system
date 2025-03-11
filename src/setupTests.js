55e075340506f8c79c09cc9540451a99112e112b
ergjustin <justin.joseph.erg.com>
2025-03-11 09:55:23 -0500

    fix: jest implemented

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,12 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 // import '@testing-library/jest-dom';
-
-
-import '@testing-library/jest-dom/vitest'
-import { beforeAll, afterEach, afterAll } from 'vitest'
-import { server } from './mocks/server'
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+import '@testing-library/jest-dom';
+import nodeCrypto from 'crypto';
+window.crypto = {
+  getRandomValues: function (buffer) {
+    return nodeCrypto.randomFillSync(buffer);
+  }
+}
--- vite.config.mts
+++ vite.config.mts
@@ -48,7 +48,7 @@ export default defineConfig({
   },
   test: {
     environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    setupFiles: './src/setupTests.ts',
     coverage: {
       reporter: ['text', 'json', 'html']
     }
