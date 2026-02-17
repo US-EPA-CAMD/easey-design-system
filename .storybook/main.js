364e4f100a0dca81a2a198b38af2b45113372c6f
Yonatan Amezene <yamezene@decisionpointcorp.com>
2026-02-17 14:39:13 -0500

    chore: upgrade Storybook from 8.6.15 to 10.2.8
    -Resolve dependabot security vulnerabilities by upgrading all Storybook packages to v10.
    -Consolidated deprecated packages into core storybook package, converted config to ESM, and added React 17 compatibility workaround.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,15 @@
-module.exports = {
+export default {
   staticDirs: ['../public'],
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-links'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
   framework: {
     name: '@storybook/react-vite',
-    options: {}
+    options: {
+      legacyRootApi: true
+    }
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
   },
   core: {
     builder: '@storybook/builder-vite',
